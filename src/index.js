a3f61a785a61f48c8ccc19a4ec699b3dfb7a9cf6
Anton Styazhkin <rudana089@gmail.com>
2023-06-07 21:32:20 +0300

    async await

--- src/index.js
+++ src/index.js
@@ -1,134 +1,66 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom/client';
+//import App from 'components/App';
+//import './index.css';
+//import fetchArticlesWithQuery from "./services/api";
+import ContentLoader from 'react-content-loader'
+import fetchArticlesWithQuery from "./services/api";
 
-import ReactDOM from 'react-dom/client'
-//import App from './components/App'
-import React, { Component } from 'react';
-import { nanoid } from "nanoid";
+const ArticleList = ({ articles }) => (
+  <ul>
+    {articles.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
-const Gender = {
-  MALE: "male",
-  FEMALE: "female",
-};
-
-const INITIAL_STATE = {
-  login: "",
-  email: "",
-  password: "",
-  agreed: false,
-  gender: null,
-  age: "",
-};
-
-class SignUpForm extends Component {
-  loginInputId = nanoid();
-  state = { ...INITIAL_STATE };
-
-  // Для всех инпутов создаем один обработчик
-  // Различать инпуты будем по атрибуту name
-  handleChange = evt => {
-    const { name, value, type, checked } = evt.target;
-    this.setState({ [name]: value });
-    this.setState({ [name]: type === "checkbox" ? checked : value });
+class App extends Component {
+  state = {
+    articles: [],
+    isLoading: false,
+    error: null,
   };
 
-  handleSubmit = evt => {
-    evt.preventDefault();
-    const { login, email, password, agreed } = this.state;
-    console.log(
-      `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}`
-    );
-    this.props.onSubmit({ ...this.state });
-    this.reset();
-  };
+  async componentDidMount() {
+    this.setState({ isLoading: true });
 
-  reset = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
+    try {
+      const articles = await fetchArticlesWithQuery("react")
+      this.setState({ articles });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
 
   render() {
-    const { login, email, password, agreed, gender, age } = this.state;
-
+    const { articles, isLoading, error } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor={this.loginInputId}>
-          Name
-          <input
-            type="text"
-            placeholder="Enter login"
-            name="login"
-            value={login}
-            id={this.loginInputId}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <section>
-          <h2>Choose your gender</h2>
-          <label>
-            Male
-            <input
-              type="radio"
-              checked={gender === Gender.MALE}
-              name="gender"
-              value={Gender.MALE}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Female
-            <input
-              type="radio"
-              checked={gender === Gender.FEMALE}
-              name="gender"
-              value={Gender.FEMALE}
-              onChange={this.handleChange}
-            />
-          </label>
-        </section>
-        <label>
-          Choose your age
-          <select name="age" value={age} onChange={this.handleChange}>
-            <option value="" disabled>
-              ...
-            </option>
-            <option value="18-25">18-25</option>
-            <option value="26-35">26-35</option>
-            <option value="36+">36+</option>
-          </select>
-        </label>
-        <label>
-          Agree to terms
-          <input
-            type="checkbox"
-            name="agreed"
-            checked={agreed}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <button type="submit" disabled={!agreed}>Sign up as {login}</button>
-      </form>
+      <div>
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+        {isLoading ? (
+          <ContentLoader viewBox="0 0 380 70">
+            {/* Only SVG shapes */}
+            <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+            <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+            <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+          </ContentLoader>
+        ) : (
+          <ArticleList articles={articles} />
+        )}
+      </div>
     );
   }
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<SignUpForm onSubmit={values => console.log(values)} />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
