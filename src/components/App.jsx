e4a5153b0e55791b0bccc7bd138bb760a0a3d20f
Anton Styazhkin <rudana089@gmail.com>
2023-06-21 17:36:49 +0300

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,34 @@
-import { Component } from 'react';
-import Header from './Header/Header';
-//import Counter from './Counter/Counter';
-import ToDoList from './ToDoList/ToDoList';
-import Modal from './Modal/Modal';
+import { Route, Routes } from 'react-router-dom';
+import { About } from '../pages/About';
+import { Home } from '../pages/Home';
+import { Products } from '../pages/Products';
+import { NotFound } from '../pages/NotFound';
+import { Container, Header, Logo, Link } from './App.styled';
 
-class App extends Component {
-  state = {
-    isShowModal: false,
-  };
-
-  showModal = () => {
-    this.setState({ isShowModal: true });
-  };
-
-  closeModal = () => {
-    this.setState({ isShowModal: false });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <Header showModal={this.showModal} />
-        {/* <Counter /> */}
-        <ToDoList />
-        {this.state.isShowModal && <Modal closeModal={this.closeModal}>Some</Modal>}
-      </div>
-    );
-  }
-}
-
-export default App;
+export const App = () => {
+  return (
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,35 +1,16 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import { useState } from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./components/App";
+import "./index.css";
 
-import { useMemo } from "react";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-const App = ({ someProp }) => {
-  const [planets,] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
-  const [query,] = useState("");
-  const [clicks, setClicks] = useState(0);
-
-  const filteredPlanets = useMemo(
-    () => planets.filter(planet => planet.includes(query)),
-    [planets, query]
-  );
-
-  return (
-    <div>
-      <div>Some prop: {someProp}</div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        Number of clicks: {clicks}
-      </button>
-      <div>
-        {filteredPlanets.map(planet => (
-          <div key={planet}>{planet}</div>
-        ))}
-      </div>
-    </div>
-  );
-};
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
