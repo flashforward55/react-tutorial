7a59dc09d579f0355dcd3a70e5e11e6f21d84e8c
Anton Styazhkin <rudana089@gmail.com>
2023-06-21 19:01:13 +0300

    About Us

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import { About } from '../pages/About';
 import { Home } from '../pages/Home';
 import { ProductDetails } from '../pages/ProductDetails';
 import { Products } from '../pages/Products';
+import { Mission } from './Mission';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
 import { Container, Header, Logo, Link } from './App.styled';
 
 export const App = () => {
@@ -25,7 +28,11 @@ export const App = () => {
       </Header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
       </Routes>
